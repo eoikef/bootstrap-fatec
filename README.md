18b285939a0813c2f26f06806855d2222b958114
Robson Hamilton <67532736+eoikef@users.noreply.github.com>
2023-11-10 17:44:32 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+Projeto Web:
+Park +
+
+Grupo:
+Cristiano Garcia Carvalhaes
+João Giacomini Sacchi
+Marcos Vinicius Batista Lourenço
+Pedro Ronned
+Robson Hamilton Carréa Filho
+
+
+
+Tecnologias utilizadas: 
+HTML5, CSS, JS e Bootstrap
+
+Como fazer alteracoes no projeto:
+Baixar o plugin do visual studio code "Live Server" para conseguir hostear o site localmente 
+e ter um feedback em tempo real das alteracoes.
+
+
+
+Estrutura do projeto:
+css: Contém os templates default do bootstrap e os templates customizados proprios do site
+
+fonts: Fontes basicas do bootstrap
+
+images: Imagens do projeto
+
+js: Todas as lógicas de click-scroll da navbar e jquery. Também possuí as validacoes de formularios de cadastro de conta.
+video: Videos do projeto
+
+As paginas html estao soltas na pasta raiz do projeto.
+
+
+
+
+
+
+
+
+
+
+fontes de pesquisa
+
+Colocar link em foto
+https://pagedart.com/blog/linking-images-in-html/
+
+Mudar tamanho do botao jquery
+https://stackoverflow.com/questions/28191573/change-buttons-to-btn-block-on-mobile-screen-size
+
+Mudar cor da fonte
+https://www.freecodecamp.org/news/how-to-change-text-color-in-html/
+
+Baixar visual studio e documentacao
+https://code.visualstudio.com/
+
+Baixar live server para conseguir ter feedback das mudancas no html e css em tempo real enquanto programa
+https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
---
+++ js/validation.js
@@ -0,0 +1,72 @@
+// js/validacao.js
+
+$(document).ready(function () {
+    // Máscara para o campo de celular
+    $('#cadastro-celular').inputmask('(99)99999-9999');
+});
+
+function validarCadastro() {
+    var email = document.getElementById('cadastro-email').value;
+    var celular = document.getElementById('cadastro-celular').value;
+    var senha = document.getElementById('cadastro-senha').value;
+    var placaCarro = document.getElementById('placa-carro-principal').value;
+
+    // Validar o formato do email
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Por favor, insira um email válido.');
+        return false;
+    }
+
+    // Validar o formato do celular
+    var celularRegex = /^\(\d{2}\)\d{5}-\d{4}$/;
+    if (!celularRegex.test(celular)) {
+        alert('Por favor, insira um número de celular válido. Ex: (11)91234-5678');
+        return false;
+    }
+
+    // Validar a senha com no mínimo 6 caracteres
+    if (senha.length < 6) {
+        alert('A senha deve ter pelo menos 6 caracteres.');
+        return false;
+    }
+
+    // Validar o formato da placa do carro
+    var placaRegex = /^[A-Za-z]{3}\d{1}[A-Za-z]\d{2}$|^[A-Za-z]{3}-\d{4}$/;
+    if (!placaRegex.test(placaCarro)) {
+        alert('Por favor, insira uma placa de carro válida.');
+        return false;
+    }
+
+        // Se todas as validações passarem, você pode enviar os dados para o backend aqui.
+        // enviarCadastroParaBackend(email, celular, senha, placaCarro);
+
+    // Redirecionar para a área logada
+    window.location.href = 'index.html#section_1'; // Substitua pelo URL da area logada
+
+    return false;
+}
+
+function enviarCadastroParaBackend(email, celular, senha, placaCarro) {
+    // Aqui você pode usar XMLHttpRequest ou outra biblioteca/função para enviar dados para o backend.
+    // Exemplo com XMLHttpRequest:
+    var xhr = new XMLHttpRequest();
+    var url = 'URL_DO_SEU_BACKEND';  // Substitua pelo URL real do seu backend.
+    var params = 'email=' + encodeURIComponent(email) +
+                 '&celular=' + encodeURIComponent(celular) +
+                 '&senha=' + encodeURIComponent(senha) +
+                 '&placaCarro=' + encodeURIComponent(placaCarro);
+    
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // A resposta do backend está disponível em xhr.responseText.
+            console.log('Resposta do Backend:', xhr.responseText);
+        }
+    }
+
+    xhr.send(params);
+}
+
